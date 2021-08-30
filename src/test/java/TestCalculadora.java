7cd644aeea4b4c98bf629a49be37a846fd8b4bf0
Lucas <lucastovo@hotmail.com>
2021-08-30 02:41:01 -0300

    testing the calculator

---
+++ src/test/java/TestCalculadora.java
@@ -0,0 +1,68 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestCalculadora {
+  public TestCalculadora(){
+
+  }
+  Calculadora calculadora = new Calculadora();
+
+  @Test
+  @DisplayName("Soma de 2 numeros inteiros positivos")
+  void somaInteirosPositivos(){
+    int result = calculadora.somar(5, 5);
+    assertEquals( 10, result);
+  }
+
+  @Test
+  @DisplayName("Soma de 2 numeros inteiros negativos")
+  void somaInteirosNegativos(){
+    int result = calculadora.somar(-5, -5);
+    assertEquals( -10, result);
+  }
+
+  @Test
+  @DisplayName("Subtração de 2 numeros inteiros positivos")
+  void subtraçãoInteirosPositivos(){
+    int result = calculadora.subtrair(5, 5);
+    assertEquals( 0, result);
+  }
+
+  @Test
+  @DisplayName("Subtração de 2 numeros inteiros negativos")
+  void subtraçãoInteirosNegativos(){
+    int result = calculadora.subtrair(-5, -5);
+    assertEquals( 0, result);
+  }
+
+  @Test
+  @DisplayName("Multiplicação de 2 numeros inteiros positivos")
+  void multiplicaInteirosPositivos(){
+    int result = calculadora.multiplicar(5, 5);
+    assertEquals( 25, result);
+  }
+
+  @Test
+  @DisplayName("Multiplicação de 2 numeros inteiros negativos")
+  void multiplicaInteirosNegativos(){
+    int result = calculadora.multiplicar(-5, -5);
+    assertEquals( 25, result);
+  }
+
+  @Test
+  @DisplayName("Divisão de 2 numeros inteiros positivos")
+  void divisãoInteirosPositivos(){
+    int result = calculadora.dividir(5, 5);
+    assertEquals( 1, result);
+  }
+
+  @Test
+  @DisplayName("Divisão de 2 numeros inteiros negativos")
+  void divisãoInteirosNegativos(){
+    int result = calculadora.dividir(-5, -5);
+    assertEquals( 1, result);
+  }
+  
+}
